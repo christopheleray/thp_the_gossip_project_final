fdbd05b213846ce7da31423da07ab92e1c8cd706
Ertemelle <phenixime@hotmail.fr>
2018-08-07 19:19:07 +0200

    tableau gossip

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -22,6 +22,12 @@ class GossipsController < ApplicationController
 		end
 	end
 
+	def destroy
+		@gossip = Gossip.find(params[:id])
+		@gossip.destroy
+		redirect_to root_path
+	end
+
 	private
 	def gossip_params
 		params.require(:gossip).permit(:content)
