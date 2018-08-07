13993a34f235b221d6dcc9c5e60c054ceac5afa0
christopheleray <christophe.leray1@gmail.com>
2018-08-07 14:15:39 +0200

    routes et css

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,3 @@
 Rails.application.routes.draw do
-  get 'moussaillons/new'
-  get 'moussaillons/create'
-  get 'moussaillons/index'
-  get 'moussaillons/show'
-  get 'moussaillons/delete'
-  get 'moussaillons/update'
-  devise_for :moussaillons
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  root to: 'moussaillons#index'
+  end
