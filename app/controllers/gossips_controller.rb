34760ac400e38e34a0e11e9ab7f7f059f0b3cdbe
Ertemelle <phenixime@hotmail.fr>
2018-08-07 16:50:35 +0200

    pb creer gossip

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -2,4 +2,26 @@ class GossipsController < ApplicationController
 	def index
 		@gossip = Gossip.all
 	end
+
+	def new
+		puts "j'affiche le new"
+		puts params.inspect
+		@gossip = Gossip.new
+	end
+
+	def create
+		puts "j'affiche le create"
+		puts params.inspect
+		@gossip = Gossip.new(gossip_params, user_id: 2)
+		if @gossip.save
+			#redirect_to gossip_create_path
+		else
+			render 'new'
+		end
+	end
+
+	private
+	def gossip_params
+		params.require(:gossip).permit(:content)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'static_pages#home'
-  get '/gossips/index', to: 'gossips#index', as: 'gossips'
+  get 'gossips/index', to: 'gossips#index', as: 'gossips'
+  get 'gossips/new', to: 'gossips#new', as: 'new_gossip'
+  post 'gossips/new', to: 'gossips#create', as: 'gossip_create'
 end
 
